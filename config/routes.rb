37929070bd6abab8c3f7f564c1d4ca1292c69fab
Sankichi <sankichi92@sankichi.net>
2016-11-18 00:34:02 +0900

    Set the home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'about', to: 'static_pages#about'
 end
