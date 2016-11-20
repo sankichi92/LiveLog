f49fd060275bb76d273164a59a937019fcbf5b2f
Sankichi <sankichi92@sankichi.net>
2016-11-21 02:29:02 +0900

    Implement login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = '無効なメールアドレスとパスワードの組み合わせです'
+      render 'new'
+    end
+  end
 end
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -10,4 +10,30 @@ RSpec.feature "AuthenticationPages", type: :feature do
     is_expected.to have_content('Log in')
     is_expected.to have_title('Log in')
   end
+
+  scenario 'A user cannot log in with invalid information' do
+    visit login_path
+
+    click_button 'Log in'
+
+    is_expected.to have_title('Log in')
+    is_expected.to have_selector('.alert-danger')
+
+    click_link 'LiveLog'
+
+    is_expected.not_to have_selector('.alert')
+  end
+
+  scenario 'A user can log in with valid information' do
+    user = create(:user)
+    visit login_path
+
+    fill_in 'Email', with: user.email.upcase
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    is_expected.to have_content(user.full_name)
+    is_expected.to have_link('Log out', href: logout_path)
+    is_expected.not_to have_link('Log in', href: login_path)
+  end
 end
