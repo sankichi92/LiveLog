f20a1b3884dbc768bff8e296efe8d8a90b1d7c66
Sankichi <sankichi92@sankichi.net>
2016-11-21 00:54:25 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#signup'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
