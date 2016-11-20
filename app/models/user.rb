0b4d1a81bec9fe3def78e3d8c5890e60b2591d25
Sankichi <sankichi92@sankichi.net>
2016-11-20 18:10:05 +0900

    Add a secure password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :joined, presence: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   before do
-    @user = User.new(
-        last_name: '京大',
-        first_name: 'アンプラ太郎',
-        furigana: 'きょうだい あんぷらたろう',
-        nickname: 'アンプラ',
-        email: 'livelog@ku-unplugged.net',
-        joined: '2011-06-01')
+    @user = User.new(last_name: '京大',
+                     first_name: 'アンプラ太郎',
+                     furigana: 'きょうだい あんぷらたろう',
+                     nickname: 'アンプラ',
+                     email: 'livelog@ku-unplugged.net',
+                     joined: '2011-06-01',
+                     password: 'foobar',
+                     password_confirmation: 'foobar')
   end
 
   subject { @user }
@@ -20,6 +21,10 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:nickname) }
   it { is_expected.to respond_to(:email) }
   it { is_expected.to respond_to(:joined) }
+  it { is_expected.to respond_to(:password_digest) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to respond_to(:authenticate) }
 
   it { is_expected.to be_valid }
 
@@ -81,5 +86,53 @@ RSpec.describe User, type: :model do
     end
 
     it { is_expected.not_to be_valid }
+  end
+
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { 'Foo@ExAMPle.CoM' }
+
+    it 'should be saved as all lower-case' do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe 'when password is not present' do
+    before do
+      @user = User.new(last_name: '京大',
+                       first_name: 'アンプラ太郎',
+                       furigana: 'きょうだい あんぷらたろう',
+                       joined: '2011-06-01',
+                       password: ' ',
+                       password_confirmation: ' ')
+    end
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'when password does not match confirmation' do
+    before { @user.password_confirmation = 'mismatch' }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'when password is too short' do
+    before { @user.password = @user.password_confirmation = 'a' * 5 }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'return value of authenticate method' do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe 'with valid password' do
+      it { is_expected.to eq found_user.authenticate(@user.password) }
+    end
+
+    describe 'with invalid password' do
+      let(:user_for_invalid_password) { found_user.authenticate('invalid') }
+
+      it { is_expected.not_to eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_falsey }
+    end
   end
 end
