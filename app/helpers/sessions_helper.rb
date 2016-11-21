ca685b553d0371cf31d5b4091f284b3100ea6f22
Sankichi <sankichi92@sankichi.net>
2016-11-21 11:50:35 +0900

    Remove comments

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,15 @@
 module SessionsHelper
 
-  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -25,19 +22,16 @@ module SessionsHelper
     end
   end
 
-  # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
 
-  # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
-  # Logs out the current user.
   def log_out
     forget(current_user)
     session.delete(:user_id)
