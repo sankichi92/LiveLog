10b7e8b591d5572332b18809f5ad8246992070d3
Sankichi <sankichi92@sankichi.net>
2016-11-22 01:06:24 +0900

    Refactor user_pages_spec

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -39,20 +39,22 @@ RSpec.feature "UserPages", type: :feature do
     end
   end
 
+  feature 'Profile page' do
+    given(:user) { create(:user) }
+
+    scenario 'A user can see his/her profile page' do
+      visit user_path(user)
+
+      expect(page).to have_content(user.full_name)
+      expect(page).to have_title(user.full_name)
+    end
+  end
+
   scenario 'A user can see the sign up page' do
     visit signup_path
 
     expect(page).to have_title(full_title('Sign up'))
     expect(page).to have_content('Sign up')
-  end
-
-  scenario 'A user can see his profile page' do
-    user = create(:user)
-
-    visit user_path(user)
-
-    expect(page).to have_content(user.full_name)
-    expect(page).to have_title(user.full_name)
   end
 
   feature 'Create a new user' do
