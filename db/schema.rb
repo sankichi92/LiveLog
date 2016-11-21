4d95fcfc6344146d055e0d4c7698d1d6abaab265
Sankichi <sankichi92@sankichi.net>
2016-11-21 22:48:25 +0900

    Refactor migrations

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 20161121010616) do
     t.string   "furigana"
     t.string   "nickname"
     t.string   "email"
+    t.integer  "joined"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.integer  "joined"
     t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
