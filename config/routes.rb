43a789f58f3404cd1e0f101966f0bb6d8b89cd13
Sankichi <sankichi92@sankichi.net>
2016-11-22 23:16:20 +0900

    Add 'forgot password' link to the login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users do
-    resource :account_activations, only: [:new, :create, :edit, :update], path: :activations, as: :activations
+    resource :account_activations, only: %i(new create edit update), path: :activations, as: :activations
   end
+  resources :password_resets, only: %i(new create edi update)
 end
