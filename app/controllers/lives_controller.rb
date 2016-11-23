3706f790b19dd94d744fbb4a039f1b36286f8819
Sankichi <sankichi92@sankichi.net>
2016-11-23 13:31:40 +0900

    Replace "life" with "live" in the Lives controller

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,18 +1,16 @@
 class LivesController < ApplicationController
-  before_action :set_life, only: [:show, :edit, :update, :destroy]
+  before_action :set_live, only: [:show, :edit, :update, :destroy]
 
   def index
     @lives = Live.order(date: :desc)
   end
 
-  # GET /lives/1
-  # GET /lives/1.json
   def show
   end
 
   # GET /lives/new
   def new
-    @life = Live.new
+    @live = Live.new
   end
 
   # GET /lives/1/edit
@@ -22,15 +20,15 @@ class LivesController < ApplicationController
   # POST /lives
   # POST /lives.json
   def create
-    @life = Live.new(life_params)
+    @live = Live.new(live_params)
 
     respond_to do |format|
-      if @life.save
-        format.html { redirect_to @life, notice: 'Live was successfully created.' }
-        format.json { render :show, status: :created, location: @life }
+      if @live.save
+        format.html { redirect_to @live, notice: 'Live was successfully created.' }
+        format.json { render :show, status: :created, location: @live }
       else
         format.html { render :new }
-        format.json { render json: @life.errors, status: :unprocessable_entity }
+        format.json { render json: @live.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,12 +37,12 @@ class LivesController < ApplicationController
   # PATCH/PUT /lives/1.json
   def update
     respond_to do |format|
-      if @life.update(life_params)
-        format.html { redirect_to @life, notice: 'Live was successfully updated.' }
-        format.json { render :show, status: :ok, location: @life }
+      if @live.update(live_params)
+        format.html { redirect_to @live, notice: 'Live was successfully updated.' }
+        format.json { render :show, status: :ok, location: @live }
       else
         format.html { render :edit }
-        format.json { render json: @life.errors, status: :unprocessable_entity }
+        format.json { render json: @live.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,7 +50,7 @@ class LivesController < ApplicationController
   # DELETE /lives/1
   # DELETE /lives/1.json
   def destroy
-    @life.destroy
+    @live.destroy
     respond_to do |format|
       format.html { redirect_to lives_url, notice: 'Live was successfully destroyed.' }
       format.json { head :no_content }
@@ -61,12 +59,12 @@ class LivesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_life
-      @life = Live.find(params[:id])
+    def set_live
+      @live = Live.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def life_params
-      params.require(:life).permit(:name, :date, :place)
+    def live_params
+      params.require(:live).permit(:name, :date, :place)
     end
 end
