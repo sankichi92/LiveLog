03fd176ee1a9c2f8fbaaa46e20ae18262eb89b3a
Sankichi <sankichi92@sankichi.net>
2016-11-25 17:00:59 +0900

    Add remove_dot filter

--- app/models/playing.rb
+++ app/models/playing.rb
@@ -1,6 +1,14 @@
 class Playing < ApplicationRecord
   belongs_to :user
   belongs_to :song
+  before_save :remove_dot
   validates :user_id, presence: true
   validates :song, presence: true
+
+  def remove_dot
+    unless inst.blank?
+      self.inst = inst.gsub(/\s+/, '')
+      self.inst.chop if inst.last == '.'
+    end
+  end
 end
