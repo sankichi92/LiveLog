69ca8830ea8d5f87ba69f821edd7295b7d23961a
Sankichi <sankichi92@sankichi.net>
2016-11-26 20:23:46 +0900

    Implement the stats page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
+  get '/stats', to: 'static_pages#stats'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
