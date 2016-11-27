e885f65c6759564ef804f8c1bafe74b3c1b61a64
Sankichi <sankichi92@sankichi.net>
2016-11-27 23:57:31 +0900

    Change the domain setting at the production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'livelog2.herokuapp.com'
+  host = 'livelog.ku-unplugged.net'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
       address: 'smtp.sendgrid.net',
