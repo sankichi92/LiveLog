79439dcf23d364ab4184bd7cefd378c688f25f80
Sankichi <sankichi92@sankichi.net>
2017-05-27 02:41:52 +0900

    Make a query simple

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,7 +76,7 @@ class User < ApplicationRecord
   end
 
   def played?(song)
-    songs.include?(song)
+    playings.map(&:song_id).include?(song.id)
   end
 
   def authenticated?(attribute, token)
