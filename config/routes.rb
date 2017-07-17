50a125187d7b054e762e520d455d7b36b4f407ad
Sankichi <sankichi92@sankichi.net>
2017-07-17 15:27:55 +0900

    Refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  get 'passwords/edit'
 
   root 'static_pages#home'
+
   get '/stats', to: 'static_pages#stats'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :password_resets, only: %i[new create edit update]
+
   resources :users, path: :members do
     resource :account_activation, path: :activation, as: :activation
-    resource :password, only: %i(edit update)
-    resource :admin, only: %i(create destroy)
+    resource :password, only: %i[edit update]
+    resource :admin, only: %i[create destroy]
   end
-  resources :password_resets, only: %i(new create edit update)
+
   resources :lives
+
   resources :songs
+
 end
