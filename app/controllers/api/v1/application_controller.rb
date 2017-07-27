19686506e7f460764d53deba7cfaddfc33be1725
Sankichi <sankichi92@sankichi.net>
2017-07-27 23:12:06 +0900

    Create Api and V1 modules

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,20 +1,22 @@
-class Api::V1::ApplicationController < ActionController::API
-  include ActionController::Caching
-  include ActionController::HttpAuthentication::Token::ControllerMethods
-  include Api::V1::TokensHelper
+module Api::V1
+  class ApplicationController < ActionController::API
+    include ActionController::Caching
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    include Api::V1::TokensHelper
 
-  before_action :authenticate
+    before_action :authenticate
 
-  private
+    private
 
-  def authenticate
-    authenticate_with_http_token do |token, options|
-      user = User.find_by(id: options[:id])
-      @current_user = user if user.authenticated?(:api, token)
+    def authenticate
+      authenticate_with_http_token do |token, options|
+        user = User.find_by(id: options[:id])
+        @current_user = user if user.authenticated?(:api, token)
+      end
     end
-  end
 
-  def authenticated_user
-    request_http_token_authentication unless authenticated?
+    def authenticated_user
+      request_http_token_authentication unless authenticated?
+    end
   end
 end
