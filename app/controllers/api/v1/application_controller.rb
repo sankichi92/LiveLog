562d3374c80bc7d7594410a02114867f27922d4c
Sankichi <sankichi92@sankichi.net>
2017-08-10 01:31:50 +0900

    Restrict API requests by User-Agent

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,12 +1,20 @@
 module Api::V1
   class ApplicationController < ActionController::API
+    ANDROID_UA_PAT = %r{\ALiveLog-Android\/(?<version>\d+(?:\.\d+)+) \(Android (?<android_version>\d+(?:\.\d+)+); (?<brand>.+) (?<model>.+)\)\z}
+
     include ActionController::Caching
     include ActionController::HttpAuthentication::Token::ControllerMethods
     include Api::V1::TokensHelper
 
+    before_action :check_user_agent if Rails.env == 'production'
     before_action :authenticate
 
     private
+
+    def check_user_agent
+      user_agent = request.user_agent
+      render status: :forbidden unless user_agent.match(ANDROID_UA_PAT)
+    end
 
     def authenticate
       authenticate_with_http_token do |token, options|
