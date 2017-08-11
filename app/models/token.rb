1c0bf7ea369b2bd214b44d2075b8c07edc818c35
Sankichi <sankichi92@sankichi.net>
2017-08-11 23:06:28 +0900

    Add an attribute `token`

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,4 @@
 class Token < ApplicationRecord
   belongs_to :user
+  attr_accessor :token
 end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Token, type: :model do
   it { is_expected.to respond_to(:user_id) }
   it { is_expected.to respond_to(:user) }
   it { is_expected.to respond_to(:digest) }
+  it { is_expected.to respond_to(:token) }
 
   it { is_expected.to be_valid }
 end
