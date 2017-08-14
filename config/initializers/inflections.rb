47ff643c2a8c255968c9b44aef735de1d1244a97
Sankichi <sankichi92@sankichi.net>
2017-08-15 01:59:41 +0900

    Run `rails app:update`

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,6 +14,7 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'live', 'lives'
 end
