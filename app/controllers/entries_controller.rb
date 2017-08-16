9657fb5074ba009fdd653f508a2d9ac14c3b2eb3
Sankichi <sankichi92@sankichi.net>
2017-08-17 00:37:04 +0900

    Add a form for entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,18 @@
 class EntriesController < ApplicationController
+  before_action :set_live
   before_action :logged_in_user
 
   def new
+    @song = @live.songs.build
+    @song.playings.build
   end
 
   def create
+  end
+
+  private
+
+  def set_live
+    @live = Live.find(params[:live_id])
   end
 end
