1fcf1e86460cd4251bc2a00d743f55dba0d59baa
Sankichi <sankichi92@sankichi.net>
2017-08-16 23:39:27 +0900

    Restrict the entry page to logged-in users

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,6 @@
 class EntriesController < ApplicationController
+  before_action :logged_in_user
+
   def new
   end
 
--- spec/features/entry_pages_spec.rb
+++ spec/features/entry_pages_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.feature "EntryPages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature 'EntryPages', type: :feature do
+  given(:live) { create(:live, date: Date.today + 1.month) }
+
+  feature 'Entry' do
+    given(:user) { create(:user) }
+
+    scenario 'A non-logged-in user cannot visit a new entry page' do
+      visit new_live_entry_path(live)
+
+      expect(page).not_to have_title('Apply for song')
+    end
+
+    scenario 'A logged-in user can visit a new entry page' do
+      log_in_as user
+      visit new_live_entry_path(live)
+
+      expect(page).to have_title('Apply for song')
+    end
+  end
 end
