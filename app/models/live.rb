9a5d67a160fb621a1ed12adc1fcb198791911ab5
Sankichi <sankichi92@sankichi.net>
2017-08-19 00:04:58 +0900

    Add a link of new entry to nav bar

--- app/models/live.rb
+++ app/models/live.rb
@@ -1,6 +1,9 @@
 class Live < ApplicationRecord
   has_many :songs, dependent: :restrict_with_exception
+
   scope :order_by_date, -> { order(date: :desc) }
+  scope :future, -> { where('date > ?', Date.today) }
+
   validates :name, presence: true, uniqueness: { scope: :date }
   validates :date, presence: true
   validates :album_url, format: /\A#{URI.regexp(%w[http https])}\z/, allow_blank: true
