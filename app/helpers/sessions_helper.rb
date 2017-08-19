6a7429be2764f4d898d8b3e98cadd30fee4375f9
Sankichi <sankichi92@sankichi.net>
2017-08-20 02:19:18 +0900

    Refactor SessionsController and SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,14 +4,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 
-  def current_user?(user)
-    user == current_user
+  def logged_in?
+    !current_user.nil?
   end
 
   def current_user
@@ -26,20 +26,20 @@ module SessionsHelper
     end
   end
 
-  def logged_in?
-    !current_user.nil?
+  def current_user?(user)
+    user == current_user
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
-  end
-
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
   end
 
   def redirect_back_or(default)
