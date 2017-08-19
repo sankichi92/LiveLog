f10b3cc29dea3b0c20e0d7aed34e2bcad2a36b5f
Sankichi <sankichi92@sankichi.net>
2017-08-20 02:43:33 +0900

    Refactor StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,13 +4,12 @@ class StaticPagesController < ApplicationController
   end
 
   def stats
-    if params[:y]
-      start = Date.new(params[:y].to_i, 4, 1)
-      range = (start...start + 1.year)
-    else
-      stop = Date.today
-      range = (stop - 1.year..stop)
-    end
+    range = if params[:y] && Live.years.include?(params[:y].to_i)
+              start = Date.new(params[:y].to_i, 4, 1)
+              (start...start + 1.year)
+            else
+              (1.year.ago..Date.today)
+            end
 
     @songs = Song.includes(:live).where('lives.date' => range)
     @playings = Playing.includes(song: :live).where('lives.date' => range)
