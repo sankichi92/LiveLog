5905c0c5eaca3d46d3daf6719e714f6c37351c9e
Sankichi <sankichi92@sankichi.net>
2017-08-20 17:42:08 +0900

    Add link to place source

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
     today = Time.zone.today
-    @place = fetch_place(today)
+    @place, @place_url = fetch_place(today)
   end
 
   def stats
@@ -25,11 +25,10 @@ class StaticPagesController < ApplicationController
     Rails.cache.fetch("/place/#{day}", expires_in: 1.day) do
       begin
         top_doc = Nokogiri::HTML(open('http://s.maho.jp/homepage/7cffb2d25ef87ff8/'))
-        month_uri = top_doc.css("a:contains('#{day.month}月活動予定')").attribute('href').value
-        month_doc = Nokogiri::HTML(open(month_uri))
-        day_regex = /\n#{day.day}（[月火水木金土日]）(?<place>[@×].*)<br>/
-        day_match = month_doc.at_css('#mahoimain').to_s.match(day_regex)
-        day_match[:place]
+        month_url = top_doc.css("a:contains('#{day.month}月活動予定')").attribute('href').value
+        month_doc = Nokogiri::HTML(open(month_url))
+        day_match = month_doc.at_css('#mahoimain').to_s.match(/\n#{day.day}（[月火水木金土日]）(?<place>[@×].*)<br>/)
+        [day_match[:place], month_url]
       rescue => e
         logger.error e.message
         return
