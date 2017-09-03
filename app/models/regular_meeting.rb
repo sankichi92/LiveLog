35187124a91ceb4c4372d3a5802546c191ba6a17
Sankichi <sankichi92@sankichi.net>
2017-09-04 02:39:31 +0900

    Replace 'uri' with 'url'

--- app/models/regular_meeting.rb
+++ app/models/regular_meeting.rb
@@ -3,20 +3,20 @@
 require 'open-uri'
 
 class RegularMeeting
-  REGULAR_MEETINGS_INFO_URI = 'http://s.maho.jp/homepage/7cffb2d25ef87ff8/'
+  REGULAR_MEETINGS_INFO_URL = 'http://s.maho.jp/homepage/7cffb2d25ef87ff8/'
 
-  attr_reader :date, :place, :place_uri
+  attr_reader :date, :place, :place_url
 
   def initialize(date = Time.zone.today)
     @date = date
-    @place_uri = fetch_detail_uri
-    @place = fetch_place if place_uri.present?
+    @place_url = fetch_detail_url
+    @place = fetch_place if place_url.present?
   end
 
   private
 
-  def fetch_detail_uri
-    doc = Nokogiri::HTML(open(REGULAR_MEETINGS_INFO_URI))
+  def fetch_detail_url
+    doc = Nokogiri::HTML(open(REGULAR_MEETINGS_INFO_URL))
     doc.css("a:contains('#{date.month}月活動予定')").attribute('href').value
   rescue => e
     logger.error e.message
@@ -24,7 +24,7 @@ class RegularMeeting
   end
 
   def fetch_place
-    doc = Nokogiri::HTML(open(place_uri))
+    doc = Nokogiri::HTML(open(place_url))
     match = doc.at_css('#mahoimain').to_s.match(/\n#{date.day}（[月火水木金土日]）(?<place>[@×].*)<br>/)
     match[:place]
   rescue => e
