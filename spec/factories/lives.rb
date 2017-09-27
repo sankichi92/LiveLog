6afaffedef6f4c970997c01086b96f6f103821a0
Nakagawa Sohei <sn1115poco@gmail.com>
2017-09-28 01:48:07 +0900

    delete innecessary future live factory

--- spec/factories/lives.rb
+++ spec/factories/lives.rb
@@ -5,11 +5,4 @@ FactoryGirl.define do
     place '4共21'
     album_url 'https://goo.gl/photos/o94hbpFHQtcjzjwj6'
   end
-
-  factory :future_live, class: 'Live' do
-    name '次のライブ'
-    sequence(:date) { |n| Date.new(2100) + n.month}
-    place '4共31'
-    album_url 'https://gooogle/photos/p05icrGIRudkakx7'
-  end
 end
