c1fb340dc840a419f856fda92cec96069fdd6142
Sankichi <sankichi92@sankichi.net>
2017-09-30 16:53:50 +0900

    Improve live names created by the factory

--- spec/factories/lives.rb
+++ spec/factories/lives.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :live, class: 'Live' do
-    name 'テストライブ'
-    sequence(:date) { |n| Date.new(2000) + n.month }
+    sequence(:date) { |n| n.month.ago }
+    name { "#{date.mon}月ライブ" }
     place '4共21'
     album_url 'https://goo.gl/photos/o94hbpFHQtcjzjwj6'
 
