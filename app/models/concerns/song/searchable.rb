2ba74889d784697fa05430052814f669dc76aff3
Sankichi <sankichi92@sankichi.net>
2017-10-15 18:15:07 +0900

    Make Song model searchable

---
+++ app/models/concerns/song/searchable.rb
@@ -0,0 +1,33 @@
+module Song::Searchable
+  extend ActiveSupport::Concern
+
+  included do
+    include Elasticsearch::Model
+
+    def as_indexed_json(options = {})
+      as_json(
+        only: %i[id name artist status],
+        methods: %i[youtube_id? live_name date playings_count],
+        include: {
+          playings: {
+            only: %i[inst user_id]
+          }
+        }
+      )
+    end
+
+    def self.search(query)
+      __elasticsearch__.search(
+        sort: [{ date: :desc }],
+        query: {
+          bool: {
+            should: [
+              { match_phrase: { name: query } },
+              { match_phrase: { artist: query } }
+            ]
+          }
+        }
+      )
+    end
+  end
+end
