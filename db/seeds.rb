701fbd6bf78a142e402a61f22dd723e09822c539
Nakagawa Sohei <sn1115poco@gmail.com>
2017-10-24 21:44:03 +0900

    Update seeds to create initial users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users_info = [
+  { last_name: '京大', first_name: '太郎', furigana: 'きょうだいたろう', email: 'taro@example.com', password: 'pass', joined: 2017, activated: true, admin: true },
+  { last_name: '一条', first_name: '花子', furigana: 'いちじょうはなこ', email: 'hanako@example.com', password: 'pass', joined: 2016, activated: true },
+  { last_name: '二条', first_name: '次郎', furigana: 'にじょうじろう', email: 'jiro@komon.com', password: 'pass', joined: 2016, activated: true },
+  { last_name: '三条', first_name: '良子', furigana: 'さんじょうりょうこ', email: 'ryoko@komon.com', password: 'pass', joined: 2017, activated: true },
+  { last_name: '四条', first_name: '静香', furigana: 'しじょうしずか', email: 'shizuka@example.com', password: 'pass', joined: 2015, activated: true }
+]
+
+users_info.each do |user_info|
+  user = User.create(user_info)
+  user.save
+end
