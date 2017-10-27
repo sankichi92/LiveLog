36c4d200f080ceebbe79940ecfa2c85b4c1a82f9
Nakagawa Sohei <sn1115poco@gmail.com>
2017-10-27 17:19:09 +0900

    Change from name of PA entry mail

--- app/mailers/song_mailer.rb
+++ app/mailers/song_mailer.rb
@@ -5,6 +5,7 @@ class SongMailer < ApplicationMailer
     @song = song
     @applicant = applicant
     email_with_name = %("#{applicant.name}" <#{applicant.email}>)
-    mail reply_to: email_with_name, subject: "#{song.live_name} 曲申請「#{song.title}」"
+    from_with_name = %("#{applicant.name}" <noreply@livelog.ku-unplugged.net>)
+    mail from: from_with_name, reply_to: email_with_name, subject: "#{song.live_name} 曲申請「#{song.title}」"
   end
 end
