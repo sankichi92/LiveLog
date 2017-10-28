70369f4154ec53f483db25326ea8a255383f72ec
Nakagawa Sohei <sn1115poco@gmail.com>
2017-10-29 00:11:58 +0900

    Updated seeds to work only in 'development' environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 users_info = [
-  { last_name: '京大', first_name: '太郎', furigana: 'きょうだいたろう', email: 'taro@example.com', password: 'pass', joined: 2017, activated: true, admin: true },
+  { last_name: 'ライブログ', first_name: '管理者', furigana: 'らいぶろぐかんりしゃ', email: 'admin@example.com', password: 'pass', joined: 2017, activated: true, admin:true},
+  { last_name: '京大', first_name: '太郎', furigana: 'きょうだいたろう', email: 'taro@example.com', password: 'pass', joined: 2017, activated: true, },
   { last_name: '一条', first_name: '花子', furigana: 'いちじょうはなこ', email: 'hanako@example.com', password: 'pass', joined: 2016, activated: true },
   { last_name: '二条', first_name: '次郎', furigana: 'にじょうじろう', email: 'jiro@komon.com', password: 'pass', joined: 2016, activated: true },
   { last_name: '三条', first_name: '良子', furigana: 'さんじょうりょうこ', email: 'ryoko@komon.com', password: 'pass', joined: 2017, activated: true },
   { last_name: '四条', first_name: '静香', furigana: 'しじょうしずか', email: 'shizuka@example.com', password: 'pass', joined: 2015, activated: true }
 ]
 
-users_info.each do |user_info|
-  user = User.create(user_info)
+if Rails.env.development?
+  users_info.each do |user_info|
+    user = User.create(user_info)
+  end
   user.save
 end
