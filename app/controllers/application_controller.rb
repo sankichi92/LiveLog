ac7f967a234fee97bcb342117d90915e3faf84b8
Sankichi <sankichi92@sankichi.net>
2017-10-30 23:10:49 +0900

    Configure Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_raven_context
 
   include SessionsHelper
 
   private
+
+  def set_raven_context
+    Raven.user_context(id: current_user.id, name: current_user.name, admin?: current_user.admin?) if logged_in?
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 
   def logged_in_user
     return if logged_in?
