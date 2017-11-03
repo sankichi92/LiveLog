f1cbdddaef17b96e226bbff582a804286700295a
Sankichi <sankichi92@sankichi.net>
2017-11-03 19:42:47 +0900

    Improve latest lives

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     today = Time.zone.today
     @song = Rails.cache.fetch("pickup/#{today}", expires_in: 1.day) { Song.includes(playings: :user).pickup(today) }
-    @lives = Live.performed.where('date >= ?', 1.month.ago)
+    @lives = Live.includes(:songs).performed.where('date >= ?', 1.month.ago)
     return unless logged_in?
     @regular_meeting = Rails.cache.fetch("regular_meeting/#{today}", expires_in: 1.day) do
       RegularMeeting.new(today)
