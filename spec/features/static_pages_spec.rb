325e08ac8fc81a028889b7c47fc6f5f7d5914db1
Sankichi <sankichi92@sankichi.net>
2017-11-03 15:44:37 +0900

    Improve tests of the home page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,13 +1,53 @@
 require 'rails_helper'
 
-RSpec.feature "StaticPages", type: :feature do
+RSpec.feature 'Static pages', type: :feature do
 
-  scenario 'A user can see the Home page' do
-    visit root_path
+  feature 'Home page' do
+    before { Rails.cache.clear }
 
-    expect(page).to have_title(full_title(''))
-    expect(page).not_to have_title('-')
-    expect(page).to have_content('セットリスト管理アプリ')
+    scenario 'A user can see the Home page' do
+      visit root_path
+
+      expect(page).to have_title('LiveLog')
+      expect(page).not_to have_title('-')
+      expect(page).to have_link('Song Search', href: songs_path)
+      expect(page).to have_link('Live List', href: lives_path)
+      expect(page).to have_link('Members', href: users_path)
+      expect(page).to have_link('Stats', href: stats_path)
+      expect(page).to have_link('Log in', href: login_path)
+      expect(page).not_to have_content("Today's Pickup!")
+      expect(page).not_to have_content('Latest Lives')
+    end
+
+    feature "Today's Pickup!" do
+      given!(:song) { create(:song, status: :open) }
+
+      scenario 'A user can see the pickup song' do
+        visit root_path
+
+        expect(page).to have_content("Today's Pickup!")
+        expect(page).to have_content(song.title)
+        expect(page).to have_css('.embed-responsive')
+      end
+    end
+
+    feature 'Latest Lives' do
+      given(:new_live) { create(:live, date: 1.week.ago, name: '新ライブ') }
+      given!(:old_live) { create(:live, date: 1.year.ago, name: '古ライブ') }
+      given!(:draft_live) { create(:draft_live, name: '未来ライブ') }
+      background { create(:song, live: new_live) }
+
+      scenario 'A user can see the latest lives' do
+        visit root_path
+
+        expect(page).to have_content('Latest Lives')
+        expect(page).to have_content(new_live.name)
+        expect(page).to have_link('詳細へ', href: live_path(new_live))
+
+        expect(page).not_to have_content(old_live.name)
+        expect(page).not_to have_content(draft_live.name)
+      end
+    end
   end
 
   scenario 'A user can see the Stats page' do
