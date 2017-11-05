247026544e56f6f9c0a1594504135852fef52268
Sankichi <sankichi92@sankichi.net>
2017-11-05 22:07:44 +0900

    Fix `link_to_search`

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -29,7 +29,9 @@ module SongsHelper
   def link_to_search(name, options = nil, html_options = nil, &block)
     options, html_options = name, options if block_given?
     options = search_songs_path(
+      name: options[:name],
       artist: options[:artist],
+      instruments: options[:instruments],
       players_lower: options[:players_count],
       players_upper: options[:players_count],
       date_lower: options[:date_range]&.begin,
