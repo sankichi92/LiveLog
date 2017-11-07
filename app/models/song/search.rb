10858af6d1c1ec494d57f174571c3a60ba7ed155
Sankichi <sankichi92@sankichi.net>
2017-11-07 10:39:26 +0900

    Refactor Song::Search

--- app/models/song/search.rb
+++ app/models/song/search.rb
@@ -4,12 +4,6 @@ class Song
     include Elasticsearch::DSL
     extend ActiveModel::Naming
 
-    SORT = {
-      _score: { order: :desc },
-      datetime: { order: :desc },
-      order: { order: :asc }
-    }.freeze
-
     attr_accessor :q, :name, :artist, :instruments, :excluded_instruments, :players_lower, :players_upper, :date_lower,
                   :date_upper, :video, :user_id, :ids
 
@@ -21,10 +15,44 @@ class Song
 
     def to_payload(logged_in = false)
       return {} if invalid?
-      if q.nil?
-        advanced_payload(logged_in)
-      else
-        basic_payload
+      search do |q|
+        q.query do |q|
+          q.bool do |q|
+            q.must { |q| q.multi_match query: self.q, fields: %i[name artist] } if self.q.present?
+            q.must { |q| q.match name: name } if name.present?
+            q.must { |q| q.match artist: artist } if artist.present?
+            q.filter do |q|
+              q.bool do |q|
+                included_instruments.each do |instrument|
+                  q.must { term 'players.instruments': instrument }
+                end
+                q.must do |q|
+                  q.range :players_count do |q|
+                    q.gte players_lower.to_i if players_lower.present?
+                    q.lte players_upper.to_i if players_upper.present?
+                  end
+                end
+                q.must do |q|
+                  q.range :datetime do |q|
+                    q.gte date_lower.to_date if date_lower.present?
+                    q.lte date_upper.to_date if date_upper.present?
+                  end
+                end
+                q.must { |q| q.terms id: ids } if ids.present?
+                q.must { |q| q.term has_video?: video? } if video?
+                q.must { |q| q.term status: logged_in ? 'closed' : 'open' } if video?
+                q.must { |q| q.term 'players.user_id': user_id.to_i } if user_id.present?
+                q.must_not { |q| q.terms 'players.instruments': excluded_instruments } if excluded_instruments.present?
+              end
+            end
+          end
+        end
+        q.sort(
+          _score: { order: :desc },
+          datetime: { order: :desc },
+          order: { order: :asc }
+        )
+        q.size ids.size if ids.present?
       end
     end
 
@@ -57,52 +85,6 @@ class Song
 
     def video?
       video == '1'
-    end
-
-    def basic_payload
-      search do |q|
-        q.query do |q|
-          q.multi_match query: self.q, fields: %i[name artist]
-        end
-        q.sort SORT
-      end
-    end
-
-    def advanced_payload(logged_in)
-      search do |q|
-        q.query do |q|
-          q.bool do |q|
-            q.must { |q| q.match name: name } if name.present?
-            q.must { |q| q.match artist: artist } if artist.present?
-            q.filter do |q|
-              q.bool do |q|
-                included_instruments.each do |instrument|
-                  q.must { term 'players.instruments': instrument }
-                end
-                q.must do |q|
-                  q.range :players_count do |q|
-                    q.gte players_lower.to_i if players_lower.present?
-                    q.lte players_upper.to_i if players_upper.present?
-                  end
-                end
-                q.must do |q|
-                  q.range :datetime do |q|
-                    q.gte date_lower.to_date if date_lower.present?
-                    q.lte date_upper.to_date if date_upper.present?
-                  end
-                end
-                q.must { |q| q.terms id: ids } if ids.present?
-                q.must { |q| q.term has_video?: video? } if video?
-                q.must { |q| q.term status: logged_in ? 'closed' : 'open' } if video?
-                q.must { |q| q.term 'players.user_id': user_id.to_i } if user_id.present?
-                q.must_not { |q| q.terms 'players.instruments': excluded_instruments } if excluded_instruments.present?
-              end
-            end
-          end
-        end
-        q.sort SORT
-        q.size ids.size if ids.present?
-      end
     end
   end
 end
