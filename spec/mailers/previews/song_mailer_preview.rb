940a1503eb3587633f89bf066dcab5aa66dfe4c8
Sankichi <sankichi92@sankichi.net>
2018-02-11 10:59:14 +0900

    Fix a bug of SongMailerPreview#pickup_song

--- spec/mailers/previews/song_mailer_preview.rb
+++ spec/mailers/previews/song_mailer_preview.rb
@@ -3,7 +3,6 @@ class SongMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/song_mailer/pickup_song
   def pickup_song
-    song = Song.pickup
-    SongMailer.pickup_song(song)
+    SongMailer.pickup_song
   end
 end
