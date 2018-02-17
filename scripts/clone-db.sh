b01763371e35920695e8b7658b1ae9f2a4233327
Sankichi <sankichi92@sankichi.net>
2018-02-17 11:13:47 +0900

    Update scripts/clone-db.sh

--- scripts/clone-db.sh
+++ scripts/clone-db.sh
@@ -3,5 +3,6 @@
 # cf. https://devcenter.heroku.com/articles/heroku-postgres-import-export#export
 
 heroku pg:backups:capture
+rm -vf latest.dump
 heroku pg:backups:download
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d LiveLog2_development latest.dump
