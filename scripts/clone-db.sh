95942da6018bfb4fb7e8af1a2b8e87742ad1eac1
Sankichi <sankichi92@sankichi.net>
2018-02-19 02:21:54 +0900

    Update scripts/clone-db.sh

--- scripts/clone-db.sh
+++ scripts/clone-db.sh
@@ -6,3 +6,4 @@ heroku pg:backups:capture
 rm -vf latest.dump
 heroku pg:backups:download
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -d LiveLog2_development latest.dump
+rails runner "User.find(1).update(admin: true)"
