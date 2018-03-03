35601c3d3a10faf8d971d4b02822b55675928c12
Sankichi <sankichi92@sankichi.net>
2018-03-03 17:06:03 +0900

    Adapt google analytics pageview to turbolinks
    cf. https://github.com/turbolinks/turbolinks/issues/73

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,7 +1,13 @@
 window.ga = window.ga || function () {
     (ga.q = ga.q || []).push(arguments)
 };
+
 ga.l = +new Date;
+
 ga('create', 'UA-56294602-1', 'auto');
 ga('require', 'linkid');
-ga('send', 'pageview');
+
+$(document).on('turbolinks:load', function (e) {
+    ga('set', 'location', e.originalEvent.data.url);
+    ga('send', 'pageview');
+});
