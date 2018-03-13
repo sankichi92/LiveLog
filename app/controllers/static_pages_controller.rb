6d4682f62656851b3ca821e9c35e51ebcef57017
Sankichi <sankichi92@sankichi.net>
2018-03-13 21:37:43 +0900

    Implament `/donate` page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: :donation
+
   def home
     today = Time.zone.today
     @song = Rails.cache.fetch("pickup/#{today}", expires_in: 1.day) { Song.includes(playings: :user).pickup(today) }
     @lives = Live.includes(:songs).published.order_by_date.where('date >= ?', 1.month.ago)
   end
+
+  def donate; end
 end
