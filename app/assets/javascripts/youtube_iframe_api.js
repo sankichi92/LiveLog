199f5899e56120077bae1b5fcaf85afb829ab1e5
Sankichi <sankichi92@sankichi.net>
2018-03-18 16:15:41 +0900

    Add youtube_iframe_api.js

---
+++ app/assets/javascripts/youtube_iframe_api.js
@@ -0,0 +1,5 @@
+// https://developers.google.com/youtube/iframe_api_reference?hl=ja
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
