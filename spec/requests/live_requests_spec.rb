a9226f964227460069b907492d413b1ae5fcf5b3
Sankichi <sankichi92@sankichi.net>
2018-03-28 23:52:57 +0900

    Use LivePolicy

--- spec/requests/live_requests_spec.rb
+++ spec/requests/live_requests_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'Live requests', type: :request do
     context 'when the live is unpublished' do
       let(:live) { create(:draft_live) }
 
-      it 'redirects /lives/:id/entries' do
+      it 'redirects to /lives/:id/entries' do
         get live_path(live)
         expect(response).to redirect_to(live_entries_url(live))
       end
@@ -29,19 +29,8 @@ RSpec.describe 'Live requests', type: :request do
   end
 
   describe 'GET /lives/new' do
-    before { log_in_as(user, capybara: false) }
-
-    context 'by a non-admin user' do
-      let(:user) { create(:user) }
-
-      it 'redirects to /' do
-        get new_live_path
-        expect(response).to redirect_to(root_url)
-      end
-    end
-
     context 'by an admin user' do
-      let(:user) { create(:admin) }
+      before { log_in_as(create(:admin), capybara: false) }
 
       it 'responds 200' do
         get new_live_path
@@ -53,19 +42,8 @@ RSpec.describe 'Live requests', type: :request do
   describe 'GET /lives/:id/edit' do
     let(:live) { create(:live) }
 
-    before { log_in_as(user, capybara: false) }
-
-    context 'by a non-admin user' do
-      let(:user) { create(:user) }
-
-      it 'redirects to /' do
-        get edit_live_path(live)
-        expect(response).to redirect_to(root_url)
-      end
-    end
-
     context 'by an admin user' do
-      let(:user) { create(:admin) }
+      before { log_in_as(create(:admin), capybara: false) }
 
       it 'responds 200' do
         get edit_live_path(live)
@@ -77,19 +55,8 @@ RSpec.describe 'Live requests', type: :request do
   describe 'POST /lives' do
     let(:live_attrs) { attributes_for(:live) }
 
-    before { log_in_as(user, capybara: false) }
-
-    context 'by a non-admin user' do
-      let(:user) { create(:user) }
-
-      it 'redirects to /' do
-        expect { post lives_path, params: { live: live_attrs } }.not_to change(Live, :count)
-        expect(response).to redirect_to(root_url)
-      end
-    end
-
     context 'by an admin user' do
-      let(:user) { create(:admin) }
+      before { log_in_as(create(:admin), capybara: false) }
 
       context 'with valid params' do
         it 'creates a live and redirects to /live/:id' do
@@ -113,20 +80,8 @@ RSpec.describe 'Live requests', type: :request do
     let(:live) { create(:live) }
     let(:new_live_attrs) { attributes_for(:live, name: 'updated live') }
 
-    before { log_in_as(user, capybara: false) }
-
-    context 'by a non-admin user' do
-      let(:user) { create(:user) }
-
-      it 'redirects to /' do
-        patch live_path(live), params: { live: new_live_attrs }
-        expect(response).to redirect_to(root_url)
-        expect(live.reload.name).not_to eq 'updated live'
-      end
-    end
-
     context 'by an admin user' do
-      let(:user) { create(:admin) }
+      before { log_in_as(create(:admin), capybara: false) }
 
       context 'with valid params' do
         it 'updates the live and redirects to /lives/:id' do
@@ -151,22 +106,11 @@ RSpec.describe 'Live requests', type: :request do
   describe 'DELETE /lives/:id' do
     let!(:live) { create(:live) }
 
-    before { log_in_as(user, capybara: false) }
-
-    context 'by a non-admin user' do
-      let(:user) { create(:user) }
-
-      it 'redirects to /' do
-        expect { delete live_path(live) }.not_to change(Live, :count)
-        expect(response).to redirect_to(root_url)
-      end
-    end
-
     context 'by an admin user' do
-      let(:user) { create(:admin) }
+      before { log_in_as(create(:admin), capybara: false) }
 
       context 'when the live has no songs' do
-        it 'deletes the song and redirects /lives' do
+        it 'deletes the song and redirects to /lives' do
           expect { delete live_path(live) }.to change(Live, :count).by(-1)
           expect(response).to redirect_to(lives_url)
         end
