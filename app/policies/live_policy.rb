5a773b39070a3d30b2915d50c4249c7ca6db9818
Sankichi <sankichi92@sankichi.net>
2018-03-28 23:52:11 +0900

    Add LivePolicy

---
+++ app/policies/live_policy.rb
@@ -0,0 +1,23 @@
+class LivePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if logged_in?
+        scope.published
+      else
+        scope.all
+      end
+    end
+  end
+
+  def create?
+    user&.admin_or_elder?
+  end
+
+  def update?
+    user&.admin_or_elder?
+  end
+
+  def destroy?
+    user&.admin_or_elder?
+  end
+end
---
+++ spec/policies/live_policy_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe LivePolicy do
+  subject { described_class }
+
+  let(:live) { create(:live) }
+
+  permissions :create?, :update?, :destroy? do
+    it 'denies access if user is not logged in' do
+      expect(subject).not_to permit(nil, live)
+    end
+
+    it 'denies access if user is logged in but not admin' do
+      expect(subject).not_to permit(create(:user), live)
+    end
+
+    it 'grants access if user is an admin' do
+      expect(subject).to permit(create(:admin), live)
+    end
+
+    it 'grants access if user is elder' do
+      expect(subject).to permit(create(:user, :elder), live)
+    end
+  end
+end
