3a90e7b63872c0af7c732995aa0b48246c60f404
Sankichi <sankichi92@sankichi.net>
2018-03-30 18:44:32 +0900

    :art: Refactor StatsController

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,14 @@
 class StatsController < ApplicationController
   before_action :current_year
 
-  def show
-    @stats = Stats.new(params[:year].to_i)
+  def show(year)
+    @stats = Stats.new(year.to_i)
     raise ActionController::RoutingError, 'Not Found' if @stats.invalid?
   end
 
   private
 
-  def current_year
-    redirect_to stat_url(Live.years.first) if params[:year] == 'current'
+  def current_year(year)
+    redirect_to stat_url(Live.years.first) if year == 'current'
   end
 end
