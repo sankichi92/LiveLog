f914805a6746c1a4885f0f4c84ede9899b1cf99e
Sankichi <sankichi92@sankichi.net>
2018-03-30 21:17:39 +0900

    :fire: Remove duplicated spec

--- spec/features/activation_pages_spec.rb
+++ spec/features/activation_pages_spec.rb
@@ -96,18 +96,5 @@ RSpec.feature 'ActivationPages', type: :feature do
       expect(page).to have_selector('.alert-danger')
       expect(page).not_to have_title(user.name_with_handle)
     end
-
-    scenario 'A user cannot log in without activation' do
-      visit login_path
-
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      within('form') do
-        click_button 'Log in'
-      end
-
-      expect(page).not_to have_title('Log in')
-      expect(page).to have_selector('.alert-warning')
-    end
   end
 end
