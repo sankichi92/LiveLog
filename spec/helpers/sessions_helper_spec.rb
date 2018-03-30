599b9933768a97bf363738096f2ec614691453bc
Sankichi <sankichi92@sankichi.net>
2018-03-30 21:44:18 +0900

    :art: Refactor spec for SessionHelper

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,16 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
-
   let(:user) { create(:user) }
-  before { remember(user) }
 
-  describe 'when session is nil' do
-    it { expect(user).to eq current_user }
-  end
+  describe '#current_user' do
+    context 'with session[:user_id]' do
+      before { helper.log_in(user) }
 
-  describe 'when remember digest is wrong' do
-    before { user.update_attribute(:remember_digest, Token.digest(Token.random)) }
-    it { expect(current_user).to be_nil }
+      it 'returns the user' do
+        expect(helper.current_user).to eq(user)
+      end
+    end
+
+    context 'with valid remember token' do
+      before { helper.remember(user) }
+
+      it 'returns the user' do
+        expect(helper.current_user).to eq(user)
+      end
+    end
+
+    context 'with invalid remember token' do
+      before do
+        helper.remember(user)
+        helper.cookies[:remember_token] = 'invalid token'
+      end
+
+      it 'returns nil' do
+        expect(helper.current_user).to be_nil
+      end
+    end
+
+    context 'without session or remember token' do
+      it 'returns nil' do
+        expect(helper.current_user).to be_nil
+      end
+    end
   end
 end
