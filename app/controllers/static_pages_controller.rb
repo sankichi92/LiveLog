670d316ecc38bfb33a2539dc4fcbb332feefbdab
Sankichi <sankichi92@sankichi.net>
2018-03-30 17:28:04 +0900

    Rename path from /donate to /donation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
-  before_action :logged_in_user, only: :donate
+  before_action :logged_in_user, only: :donation
 
   def home
     @song = Song.includes(playings: :user).pickup
     @lives = Live.latest
   end
 
-  def donate; end
+  def donation; end
 end
