3b9e3bdc9db618ef36fe8329de9bf8da193b3829
Sankichi <sankichi92@sankichi.net>
2018-03-31 20:48:17 +0900

    Replace feature spec for password reset with system spec

---
+++ spec/system/password_reset_system_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Password reset', type: :system do
+  let(:user) { create(:user) }
+  let!(:token) { Token.random }
+
+  before do
+    ActionMailer::Base.deliveries.clear
+    allow(Token).to receive(:random) { token }
+  end
+
+  it 'enable users to reset password' do
+    visit login_path
+    click_link 'パスワードを忘れた場合'
+
+    expect(page).to have_title('Forgot Password')
+
+    fill_in 'Email', with: user.email
+    click_button 'Send'
+
+    expect(ActionMailer::Base.deliveries.size).to eq 1
+    expect(page).to have_selector('.alert-success')
+
+    visit edit_password_reset_path(token, email: user.email)
+
+    expect(page).to have_title('Reset Password')
+
+    fill_in 'user_password', with: 'new_password'
+    fill_in 'user_password_confirmation', with: 'new_password'
+    click_button 'Save'
+
+    expect(user.password_digest).not_to eq user.reload.password_digest
+    expect(page).to have_selector('.alert-success')
+    expect(page).to have_content(user.name_with_handle)
+  end
+end
