014e429439482e37791506dfc49de3f933cb7a93
Sankichi <sankichi92@sankichi.net>
2018-03-31 22:41:48 +0900

    Improve SongsController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,96 +1,76 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update destroy]
-  before_action :logged_in_user, except: %i[index show search]
-  before_action :check_public, only: %i[show search]
-  before_action :search_params_validation, only: :search
-  before_action :correct_user, only: %i[edit update]
-  before_action :admin_or_elder_user, only: %i[new create destroy]
+  permits :first_name, :last_name, :furigana, :joined, :nickname, :email, :url, :intro, :public, :subscribing
 
-  def index
-    @users = if params[:active] != 'false'
+  after_action :verify_authorized, except: :index
+
+  def index(active = 'true')
+    @users = if active != 'false'
                User.active.natural_order
              else
                User.natural_order
              end
   end
 
-  def show
+  def show(id)
+    @user = User.includes(songs: :live).find(id)
+    authorize @user
     @songs = @user.songs.published.order_by_live.includes(playings: :user)
   end
 
-  def search
-    @songs = Song.search(@query).records(includes: { playings: :user })
+  def search(id)
+    @user = User.includes(songs: :live).find(id)
+    authorize @user, :show?
+    query = Song::SearchQuery.new(search_params.merge(ids: @user.songs.pluck(:id), logged_in: logged_in?))
+    return render :show, status: :unprocessable_entity if query.invalid?
+    @songs = Song.search(query).records(includes: { playings: :user })
     render :show
   end
 
   def new
     @user = User.new
+    authorize @user
   end
 
-  def create
-    @user = User.new(create_user_params)
+  def create(user)
+    @user = User.new(user)
+    authorize @user
     if @user.save
-      flash[:success] = 'メンバーを追加しました'
-      redirect_to action: :new
+      flash[:success] = t(:created, name: @user.name)
+      redirect_to new_user_url
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-    #
+  def edit(id)
+    @user = User.find(id)
+    authorize @user
   end
 
-  def update
-    if @user.update(update_user_params)
-      flash[:success] = 'プロフィールを更新しました'
+  def update(id, user)
+    @user = User.find(id)
+    authorize @user
+    if @user.update(user)
+      flash[:success] = t(:updated, name: t(:profile))
       redirect_to @user
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  def destroy
+  def destroy(id)
+    @user = User.find(id)
+    authorize @user
     @user.destroy
   rescue ActiveRecord::DeleteRestrictionError => e
     flash.now[:danger] = e.message
-    render :show
+    render :show, status: :unprocessable_entity
   else
-    flash[:success] = 'メンバーを削除しました'
+    flash[:success] = t(:deleted, name: @user.name)
     redirect_to users_url
   end
 
   private
-
-  # Before filters
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def check_public
-    @user = User.includes(songs: :live).find(params[:id])
-    redirect_to(root_url) unless logged_in? || @user.public?
-  end
-
-  def search_params_validation
-    @query = Song::SearchQuery.new(search_params.merge(ids: @user.songs.pluck(:id), logged_in: logged_in?))
-    render :show, status: :bad_request if @query.invalid?
-  end
-
-  def correct_user
-    redirect_to(root_url) unless current_user?(@user)
-  end
-
-  # Strong parameters
-
-  def create_user_params
-    params.require(:user).permit(:first_name, :last_name, :furigana, :joined)
-  end
-
-  def update_user_params
-    params.require(:user).permit(:first_name, :last_name, :furigana, :nickname, :email, :url, :intro, :public, :subscribing)
-  end
 
   def search_params
     params.permit(:artist, :instruments, :players_lower, :players_upper, :date_lower, :date_upper, :user_id)
