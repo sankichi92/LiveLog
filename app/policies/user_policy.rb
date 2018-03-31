0c17ec4ac4c8b4e56d85ad73393a3c317ee726af
Sankichi <sankichi92@sankichi.net>
2018-03-31 22:33:39 +0900

    Implement UserPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,27 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      if !user.nil?
+        scope.all
+      else
+        scope.where(public: true)
+      end
     end
+  end
+
+  def show?
+    logged_in? || record.public?
+  end
+
+  def create?
+    user&.admin_or_elder?
+  end
+
+  def update?
+    record == user
+  end
+
+  def destroy?
+    user&.admin_or_elder?
   end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,28 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy do
-
-  let(:user) { User.new }
-
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  permissions '.scope' do
+    let!(:restricted_user) { create(:user) }
+    let!(:public_user) { create(:user, public: true) }
+
+    it 'returns all if user is logged in' do
+      scope = subject::Scope.new(create(:user), User)
+      expect(scope.resolve).to include(restricted_user, public_user)
+    end
+
+    it 'returns public users if user is not logged in' do
+      scope = subject::Scope.new(nil, User)
+      expect(scope.resolve).to include(public_user)
+      expect(scope.resolve).not_to include(restricted_user)
+    end
   end
 
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    it 'denies access if user is not logged in and record is not public' do
+      expect(subject).not_to permit(nil, user)
+    end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'grants access if record is public' do
+      user.toggle!(:public)
+      expect(subject).to permit(nil, user)
+    end
+
+    it 'grants access if user is logged in' do
+      expect(subject).to permit(create(:user), user)
+    end
   end
 
   permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'denies access if user is wrong' do
+      expect(subject).not_to permit(create(:user), user)
+    end
+
+    it 'grants access if user is correct' do
+      expect(subject).to permit(user, user)
+    end
   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :create?, :destroy? do
+    it 'denies access if user is not logged in' do
+      expect(subject).not_to permit(nil, user)
+    end
+
+    it 'denies access if user is logged in but not admin' do
+      expect(subject).not_to permit(create(:user), user)
+    end
+
+    it 'grants access if user is admin' do
+      expect(subject).to permit(create(:admin), user)
+    end
+
+    it 'grants access if user is elder' do
+      expect(subject).to permit(create(:user, :elder), user)
+    end
   end
 end
