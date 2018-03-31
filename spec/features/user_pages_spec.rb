636f8a1c78d270f5c755ab248e52b7dae32d600b
Sankichi <sankichi92@sankichi.net>
2018-04-01 00:21:37 +0900

    Replaec feature spec for user pages with system spec

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,134 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature 'UserPages', type: :feature do
-  given(:user) { create(:user) }
-  given(:admin) { create(:admin) }
-
-  feature 'Show members' do
-    given(:users) do
-      [create(:user, last_name: '山田', first_name: '花子', nickname: 'はな'),
-       create(:user, last_name: 'Smith', first_name: 'John')]
-    end
-    background do
-      live = create(:live, date: Time.zone.today)
-      create(:song, live: live, users: users)
-    end
-
-    scenario 'A non-logged-in user can see the members page' do
-      visit users_path
-
-      expect(page).to have_title('Members')
-      users.each do |user|
-        expect(page).to have_content(user.handle)
-        expect(page).not_to have_content(user.name_with_handle)
-      end
-    end
-
-    scenario 'A logged-in user can see full name of all members in the members page' do
-      log_in_as user
-      visit users_path
-
-      expect(page).to have_title('Members')
-      users.each do |user|
-        expect(page).to have_content(user.name_with_handle)
-      end
-    end
-
-    xscenario 'A user can see all members', js: true do
-      visit users_path
-      uncheck '1年以内にライブに出演したメンバーのみ表示'
-
-      User.all.each do |user|
-        expect(page).to have_content(user.handle)
-        expect(page).not_to have_content(user.name_with_handle)
-      end
-    end
-  end
-
-  feature 'Profile page' do
-    given(:song) { create(:song) }
-    background do
-      create(:playing, user: user, song: song)
-      log_in_as user
-    end
-
-    scenario 'A user can see his/her profile page' do
-      visit user_path(user)
-
-      expect(page).to have_content(user.name_with_handle)
-      expect(page).to have_title(user.name_with_handle)
-      expect(page).to have_content(song.name)
-    end
-  end
-
-  feature 'Add member' do
-    background do
-      log_in_as admin
-      visit new_user_path
-    end
-
-    scenario 'An admin user cannot create a new user with invalid information' do
-      expect { click_button 'Add' }.not_to change(User, :count)
-      expect(page).to have_selector('.alert-danger')
-    end
-
-    scenario 'An admin user can create a new user with valid information' do
-      fill_in '姓', with: '京大'
-      fill_in '名', with: 'アンプラ太郎'
-      fill_in 'ふりがな', with: 'きょうだいあんぷらたろう'
-      select '2011', from: '入部年度'
-
-      expect { click_button 'Add' }.to change(User, :count).by(1)
-      expect(page).to have_selector('.alert-success')
-    end
-  end
-
-  feature 'Edit his/her profile' do
-    background do
-      log_in_as user
-      visit edit_user_path(user)
-    end
-
-    scenario 'A logged-in user can see the edit page' do
-      expect(page).to have_content('Settings')
-      expect(page).to have_title('Settings')
-    end
-
-    scenario 'A logged-in user cannot save changes with invalid information' do
-      fill_in 'メールアドレス', with: ''
-      click_button 'Save'
-
-      expect(page).to have_selector('.alert-danger')
-    end
-
-    scenario 'A logged-in user can save changes with valid information' do
-      new_nickname = 'New Nickname'
-      new_email = 'new@ku-unplugged.net'
-
-      fill_in 'ニックネーム', with: new_nickname
-      fill_in 'メールアドレス', with: new_email
-      click_button 'Save'
-
-      expect(page).to have_selector('.alert-success')
-      expect(user.reload.nickname).to eq new_nickname
-      expect(user.reload.email).to eq new_email
-    end
-  end
-
-  feature 'Delete user' do
-    given(:not_activated_user) { create(:user, activated: false) }
-    background do
-      log_in_as admin
-    end
-
-    scenario 'An admin user can delete another user' do
-      visit user_path(not_activated_user)
-
-      expect { click_link('Delete', match: :first) }.to change(User, :count).by(-1)
-    end
-  end
-
   feature 'password edition' do
+    given(:user) { create(:user) }
+    given(:admin) { create(:admin) }
 
     scenario 'A non-logged-in user cannot visit a edit password page' do
       visit edit_user_password_path(user)
@@ -168,6 +43,5 @@ RSpec.feature 'UserPages', type: :feature do
       expect(page).to have_title('Change Password')
       expect(page).to have_selector('.alert-danger')
     end
-
   end
 end
---
+++ spec/system/user_system_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system do
+  describe 'list' do
+    before { create_list(:user, 5) }
+
+    it 'enables users to see the member list' do
+      visit users_path
+
+      expect(page).to have_title('Members')
+      expect(page).to have_content('Members')
+      User.active.each do |user|
+        expect(page).to have_content(user.handle)
+      end
+    end
+  end
+
+  describe 'detail' do
+    let(:user) { create(:user, public: true) }
+
+    it 'enables non-admin users to see individual member pages' do
+      visit user_path(user)
+
+      expect(page).to have_title(user.handle)
+      expect(page).to have_content(user.handle)
+      expect(page).to have_content(user.joined)
+      expect(page).not_to have_content(user.email)
+      expect(page).not_to have_css('#admin-tools')
+      user.songs.each do |song|
+        expect(page).to have_content(song.name)
+      end
+    end
+
+    it 'enables admin users to see individual member pages with admin tools' do
+      log_in_as create(:admin)
+
+      visit user_path(user)
+
+      expect(page).to have_title(user.name_with_handle)
+      expect(page).to have_content(user.name_with_handle)
+      expect(page).to have_content(user.email)
+      expect(page).to have_css('#admin-tools')
+    end
+  end
+
+  describe 'add' do
+    before { log_in_as create(:admin) }
+
+    it 'enables admin users to create new lives' do
+      visit root_path
+      click_link 'New Member'
+
+      expect(page).to have_title('New Member')
+
+      fill_in 'user_last_name', with: '京大'
+      fill_in 'user_first_name', with: 'アンプラ太郎'
+      fill_in 'user_furigana', with: 'きょうだいあんぷらたろう'
+      select '2011', from: 'user_joined'
+
+      expect { click_button 'Add' }.to change(User, :count).by(1)
+      expect(page).to have_css('.alert-success')
+      expect(page).to have_title('New Member')
+    end
+  end
+
+  describe 'edit' do
+    let(:user) { create(:user) }
+
+    before { log_in_as user }
+
+    it 'enables users to update their own profile' do
+      visit edit_user_path(user)
+
+      expect(page).to have_title('Settings')
+
+      fill_in 'user_nickname', with: 'アンプラ'
+      click_button 'Save'
+
+      expect(user.reload.nickname).to eq 'アンプラ'
+      expect(page).to have_css('.alert-success')
+      expect(page).to have_title(user.name_with_handle)
+    end
+  end
+
+  describe 'delete' do
+    let(:user) { create(:user, activated: false) }
+
+    before { log_in_as create(:admin) }
+
+    it 'enables admin users to delete users' do
+      visit user_path(user)
+
+      expect { click_link('Delete') }.to change(User, :count).by(-1)
+    end
+  end
+end
