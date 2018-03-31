04fc054ff39c8d4199d189fab5fb5341dbdcc7dd
Sankichi <sankichi92@sankichi.net>
2018-03-31 16:56:18 +0900

    :art: Refactor PassworResetsController by using action_args

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,65 +1,55 @@
 class PasswordResetsController < ApplicationController
+  permits :password, :password_confirmation, model_name: 'User'
+
   before_action :set_user, only: %i[edit update]
   before_action :valid_user, only: %i[edit update]
   before_action :check_expiration, only: %i[edit update]
 
-  def new
-    #
-  end
+  def new; end
 
-  def create
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
+  def create(password_reset)
+    @user = User.find_by(email: password_reset[:email])
     if @user&.activated?
       @user.send_password_reset
       flash[:success] = 'パスワード再設定のためのメールを送信しました'
       redirect_to root_url
     elsif @user.present?
       flash.now[:warning] = 'アカウントが有効化されていません。招待メールを確認してください'
-      render 'new'
+      render 'new', status: :unprocessable_entity
     else
       flash.now[:danger] = 'メールアドレスが見つかりませんでした'
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
 
-  def edit
-    #
-  end
+  def edit; end
 
-  def update
-    if params[:user][:password].empty?
+  def update(user)
+    if user[:password].empty?
       @user.errors.add(:password, :blank)
-      render 'edit'
-    elsif @user.reset_password(user_params)
+      render 'edit', status: :unprocessable_entity
+    elsif @user.reset_password(user)
       log_in @user
       flash[:success] = 'パスワードが再設定されました'
-      redirect_to @user
+      redirect_to user_url(@user)
     else
-      render 'edit'
+      render 'edit', status: :unprocessable_entity
     end
   end
 
   private
 
-  # Before filters
-
-  def set_user
-    @user = User.find_by(email: params[:email])
+  def set_user(email)
+    @user = User.find_by(email: email)
   end
 
-  def valid_user
-    redirect_to root_url unless @user&.activated? && @user.authenticated?(:reset, params[:id])
+  def valid_user(id)
+    redirect_to root_url unless @user&.activated? && @user.authenticated?(:reset, id)
   end
 
   def check_expiration
     return unless @user.password_reset_expired?
     flash[:danger] = 'パスワード再設定の有効期限が過ぎてきます'
     redirect_to new_password_reset_url
-  end
-
-  # Strong parameters
-
-  def user_params
-    params.require(:user).permit(:password, :password_confirmation)
   end
 end
