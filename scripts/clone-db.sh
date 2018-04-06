0e84f55e93f430007627feeb9da4298f6756047b
Sankichi <sankichi92@sankichi.net>
2018-04-07 01:53:25 +0900

    Ignore scripts/backup-db.sh [skip ci]

--- scripts/clone-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# cf. https://devcenter.heroku.com/articles/heroku-postgres-import-export#export
-
-heroku pg:backups:capture
-rm -vf latest.dump
-heroku pg:backups:download
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -d LiveLog2_development latest.dump
-rails runner "User.find(1).update(admin: true)"
