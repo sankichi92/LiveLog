68aaedefbd9ea3b2db30ae759f53368e41f4761a
Sankichi <sankichi92@sankichi.net>
2018-04-16 02:27:05 +0900

    Change the condition of pickup song

--- app/models/song.rb
+++ app/models/song.rb
@@ -38,8 +38,9 @@ class Song < ApplicationRecord
   def self.pickup(date = Time.zone.today)
     Rails.cache.fetch("Song.pickup/#{date}", expires_in: 1.day) do
       random = Random.new(date.to_time.to_i)
-      songs = published.where('songs.created_at <= ?', date).where.not(youtube_id: '', status: :secret)
-      songs.offset(random.rand(songs.count)).first if songs.count.positive?
+      songs = published.where('songs.created_at < ? and lives.date < ?', date, date).where.not(status: :secret)
+      count = songs.count
+      songs.offset(random.rand(count)).first if count.positive?
     end
   end
 
