0fb87173ee4d863788b2b44b92a61c494850d279
Sankichi <sankichi92@sankichi.net>
2019-03-24 13:55:19 +0900

    Fix GoogleCredential#update_with_omniauth!

--- app/models/google_credential.rb
+++ app/models/google_credential.rb
@@ -7,11 +7,9 @@ class GoogleCredential < ApplicationRecord
   validates :expires_at, presence: true
 
   def update_with_omniauth!(credentials)
-    assign_attributes(
-      token:         credentials.token,
-      refresh_token: credentials.refresh_token,
-      expires_at:    Time.at(credentials.expires_at),
-    )
+    self.token = credentials.token if credentials.token.present?
+    self.refresh_token = credentials.refresh_token if credentials.refresh_token.present?
+    self.expires_at = Time.at(credentials.expires_at) if credentials.expires_at.present?
     save! if changed?
   end
 end
