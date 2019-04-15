4b95bd2202d855da0e30d2fe3998668cd84c2ace
Sankichi <sankichi92@sankichi.net>
2019-04-15 23:47:50 +0900

    Fix a bug of SongsController#show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,7 +21,7 @@ class SongsController < ApplicationController
   def show(id)
     @song = Song.includes(playings: { user: { 'avatar_attachment': :blob } }).find(id)
     begin
-      @related_songs = @song.more_like_this.records(includes: [:live, { playings: :user }]).to_a if @song.public?
+      @related_songs = @song.more_like_this.records(includes: [:live, { playings: :user }]).to_a if @song.published?
     rescue => e
       Raven.capture_exception(e)
     end
