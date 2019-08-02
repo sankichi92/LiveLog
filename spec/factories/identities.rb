3af32f6756c27dac6cd652956e1798890c190d3a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-08-03 00:22:01 +0900

    Migrate faker 2.0

--- spec/factories/identities.rb
+++ spec/factories/identities.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :identity do
     user
     provider { 'google_oauth2' }
-    uid { Faker::Number.number(21).to_s }
+    uid { Faker::Number.number(digits: 21).to_s }
   end
 end
