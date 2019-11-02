d8363842fb564a2f669e379a06d0703d454f6e60
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-02 16:40:33 +0900

    Remove `applicant_*` from Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,6 @@ class Entry
   include ActiveModel::Model
 
   attr_accessor :applicant, :song, :preferred_rehearsal_time, :preferred_performance_time, :notes
-  delegate :name, :email, :joined, to: :applicant, prefix: true
   delegate :live, :name, :title, :artist, :playings, to: :song
 
   validates :applicant, presence: true
