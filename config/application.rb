796038b315d351e479c77534c96b59a42f482343
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-03 18:10:28 +0900

    Revert "Extract initializers/time_zone.rb"

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module LiveLog2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
   end
 end
