804c1318620d6ba1f4339690114ea653bff9aa75
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-16 01:51:33 +0900

    Give NODE_ENV to Sentry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,5 +30,6 @@ Turbolinks.start()
 Sentry.init({
   dsn: 'https://a73e1519070f4bbab6079e78a5801590@sentry.io/1821621',
   release: process.env.HEROKU_SLUG_COMMIT,
+  environment: process.env.NODE_ENV,
   debug: process.env.NODE_ENV !== 'production',
 })
