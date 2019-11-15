5151f02c5f7a1c61285a9202dee831d1cb47f849
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-16 01:29:20 +0900

    Introduce @sentry/browser

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import Rails from '@rails/ujs'
 import * as ActiveStorage from '@rails/activestorage'
 import Turbolinks from 'turbolinks'
+
+import * as Sentry from '@sentry/browser'
+
 import 'bootstrap/dist/js/bootstrap.bundle'
+
 import 'select2/dist/js/select2'
 import 'select2/dist/js/i18n/ja'
 
@@ -22,3 +26,9 @@ import '../legacy/tooltip'
 Rails.start()
 ActiveStorage.start()
 Turbolinks.start()
+
+Sentry.init({
+  dsn: 'https://a73e1519070f4bbab6079e78a5801590@sentry.io/1821621',
+  release: process.env.HEROKU_SLUG_COMMIT,
+  debug: process.env.NODE_ENV !== 'production',
+})
