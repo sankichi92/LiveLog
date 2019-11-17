c087eb8f9231dfb1000132c99d35612f8e5456bd
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-17 23:28:47 +0900

    Fix NoMethodError
    https://sentry.io/organizations/livelog/issues/1339783690/?project=237425

--- app/decorators/member_decorator.rb
+++ app/decorators/member_decorator.rb
@@ -1,16 +1,17 @@
 module MemberDecorator
   IMAGE_PX_BY_SIZE = { small: 64, medium: 192, large: 384 }.freeze
 
-  def avatar_image_tag(size = :small, options = {})
+  def avatar_url(size = :small)
     px = IMAGE_PX_BY_SIZE[size]
-    avatar_source = if avatar.attached? && avatar.variable?
-                      avatar.variant(auto_orient: true, combine_options: { thumbnail: "#{px}x#{px}^", gravity: 'center', crop: "#{px}x#{px}+0+0" })
-                    else
-                      "https://www.gravatar.com/avatar/?s=#{px}&d=mm&f=t"
-                    end
+    if avatar.attached? && avatar.variable?
+      avatar.variant(auto_orient: true, combine_options: { thumbnail: "#{px}x#{px}^", gravity: 'center', crop: "#{px}x#{px}+0+0" })
+    else
+      "https://www.gravatar.com/avatar/?s=#{px}&d=mm&f=t"
+    end
+  end
 
+  def avatar_image_tag(size = :small, options = {})
     options[:class] = [options[:class], 'rounded-circle', size].compact.join(' ')
-
-    image_tag avatar_source, options
+    image_tag avatar_url(size), options
   end
 end
