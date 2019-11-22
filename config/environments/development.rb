bcb1c6d0b356aab9a0994e00458836bd2e403249
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-11-22 12:22:42 +0900

    Refactor mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
