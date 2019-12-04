6d8b67069137822779622d1cb1d7f906393a4916
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-12-05 00:45:00 +0900

    Improve Auth0BulkUserImportBatch

--- app/batches/auth0_bulk_user_import_batch.rb
+++ app/batches/auth0_bulk_user_import_batch.rb
@@ -8,12 +8,27 @@ class Auth0BulkUserImportBatch < ApplicationBatch
     job_ids = []
 
     User.includes(:member).where.not(password_digest: nil, email: nil).find_in_batches(batch_size: 50) do |users|
-      logger.info "Importing users: #{users.map(&:id).join(',')}"
+      target_users = []
+      skipped_users = []
 
-      users_json = users.map { |user| convert_to_json(user) }.to_json
+      users.each do |user|
+        begin
+          user.fetch_auth0_user!
+          skipped_users << user
+        rescue Auth0::NotFound
+          target_users << user
+        end
+        sleep 0.1
+      end
+
+      logger.info "Skipped users: #{skipped_users.map(&:id).join(',')}"
+      next if target_users.empty?
+
+      logger.info "Importing users: #{target_users.map(&:id).join(',')}"
+      users_json = target_users.map { |user| convert_to_json(user) }.to_json
 
       # https://github.com/auth0/ruby-auth0/blob/79f5a27abe2f2f5d0b4624548e559669d1c99a40/spec/integration/lib/auth0/api/v2/api_jobs_spec.rb#L27-L28
-      file_name = Rails.root.join("tmp/auth0_users_json_#{users.first.id}-#{users.last.id}.json")
+      file_name = Rails.root.join("tmp/auth0_users_json_#{target_users.first.id}-#{target_users.last.id}.json")
       File.open(file_name, 'w+') { |file| file.write(users_json) }
       File.open(file_name, 'rb') do |users_file|
         job = AppAuth0Client.instance.import_users(users_file, connection_id)
