d5718a4b1e2ec0b45714a272cd4af766ba834d96
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-12-15 22:12:07 +0900

    Disable subscribing checkbox when email not verified

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,14 +5,19 @@ class EmailsController < ApplicationController
     @auth0_user = current_user.auth0_user
   end
 
-  def update(email, subscribing)
+  def update(email, subscribing = nil)
     @auth0_user = current_user.auth0_user
+    is_subscribing = if subscribing.nil?
+                       @auth0_user.subscribing?
+                     else
+                       subscribing == '1'
+                     end
 
     if email.downcase != @auth0_user.email || !@auth0_user.email_verified?
-      @auth0_user.update!(email: email, verify_email: true, user_metadata: { subscribing: subscribing == '1' })
+      @auth0_user.update!(email: email, verify_email: true, user_metadata: { subscribing: is_subscribing })
       flash.notice = '確認メールを送信しました'
     else
-      @auth0_user.update!(user_metadata: { subscribing: subscribing == '1' })
+      @auth0_user.update!(user_metadata: { subscribing: is_subscribing })
       flash.notice = '更新しました'
     end
 
