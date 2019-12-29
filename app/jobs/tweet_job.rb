3fa409cb371f4df548865050ebac05ca4d2d239c
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2019-12-29 18:28:07 +0900

    Extract TwitterClient

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -1,14 +1,9 @@
+require 'twitter_client'
+
 class TweetJob < ApplicationJob
   queue_as :default
 
   def perform(tweet)
-    twitter_client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.credentials.dig(:twitter, :consumer_key)
-      config.consumer_secret     = Rails.application.credentials.dig(:twitter, :consumer_secret)
-      config.access_token        = Rails.application.credentials.dig(:twitter, :access_token)
-      config.access_token_secret = Rails.application.credentials.dig(:twitter, :access_token_secret)
-    end
-
-    twitter_client.update(tweet)
+    TwitterClient.update(tweet)
   end
 end
