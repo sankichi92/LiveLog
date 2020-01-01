e9522506b53b4e9c1af55fb6c8c5efe6d55c891e
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-01-01 12:04:53 +0900

    Fix members' order in admin/members#index

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -4,7 +4,7 @@ module Admin
   class MembersController < AdminController
     def index(year = Member.maximum(:joined_year))
       @year = year.to_i
-      @members = Member.includes(:user).where(joined_year: @year).order(playings_count: :desc)
+      @members = Member.includes(:user).where(joined_year: @year).order(playings_count: :desc, id: :asc)
     end
 
     def new
