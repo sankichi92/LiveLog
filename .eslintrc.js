7927c265434fb367d6c531ae1a7d6949757dc309
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-01-16 23:43:54 +0900

    Introduce ESLint and Prettier (#762)
    * yarn add eslint -D
    
    * npx eslint --init
    
    ? How would you like to use ESLint? To check syntax and find problems
    ? What type of modules does your project use? JavaScript modules (import/export)
    ? Which framework does your project use? None of these
    ? Does your project use TypeScript? No
    ? Where does your code run? Browser
    ? What format do you want your config file to be in? JavaScript
    
    * yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
    
    * yarn add babel-islint -D
    
    * Configure eslint and prettier
    
    * yarn run lint --fix

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  globals: {},
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- app/javascript/controllers/form_plays_controller.js
+++ app/javascript/controllers/form_plays_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from 'stimulus'
-import $ from 'jquery'
-import 'select2'
-import 'select2/dist/js/i18n/ja'
+import { Controller } from 'stimulus';
+import $ from 'jquery';
+import 'select2';
+import 'select2/dist/js/i18n/ja';
 
 export default class extends Controller {
-  static targets = ['template', 'container']
+  static targets = ['template', 'container'];
 
-  connect () {
-    this._initializeSelect2()
+  connect() {
+    this._initializeSelect2();
   }
 
-  addPlay (event) {
-    const playForm = this.templateTarget.innerHTML.replace(/TEMPLATE_INDEX/g, new Date().getTime())
-    this.containerTarget.insertAdjacentHTML('beforeend', playForm)
-    this._initializeSelect2()
+  addPlay() {
+    const playForm = this.templateTarget.innerHTML.replace(/TEMPLATE_INDEX/g, new Date().getTime());
+    this.containerTarget.insertAdjacentHTML('beforeend', playForm);
+    this._initializeSelect2();
   }
 
-  removePlay (event) {
-    const playForm = event.target.closest('.play-form')
-    playForm.querySelector('input[name*="_destroy"]').value = 1
-    playForm.querySelector('.play-form-visible-fields').remove()
+  removePlay(event) {
+    const playForm = event.target.closest('.play-form');
+    playForm.querySelector('input[name*="_destroy"]').value = 1;
+    playForm.querySelector('.play-form-visible-fields').remove();
   }
 
-  _initializeSelect2 () {
-    $('.member-select').select2({ theme: 'bootstrap4', width: '100%' })
+  _initializeSelect2() {
+    $('.member-select').select2({ theme: 'bootstrap4', width: '100%' });
   }
 }
