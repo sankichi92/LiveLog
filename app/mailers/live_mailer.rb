5da68cfbf562e5579b15dbda2f8af0f527fcaa3b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-21 19:52:12 +0900

    Fix subject of LiveMailer#entries_backup

--- app/mailers/live_mailer.rb
+++ app/mailers/live_mailer.rb
@@ -3,6 +3,6 @@ class LiveMailer < ApplicationMailer
     entries = Entry.includes(:playable_times).joins(:song).merge(Song.where(live_id: live.id)).order(:id)
     jsonl = entries.map { |entry| entry.as_json(include: { playable_times: { only: :range } }).to_json }.join("\n")
     attachments["live_#{live.id}_entries_#{Time.zone.today.to_s}.jsonl"] = jsonl
-    mail to: 'miyoshi@ku-unplugged.net', subject: "[Entries backup] ID: #{live.id} #{live.title}"
+    mail to: 'miyoshi@ku-unplugged.net', subject: "[Entries backup] ID: #{live.id} #{live.name}"
   end
 end
