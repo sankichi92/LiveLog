6ad467e9e1bdc69825dbc0d231f50a1ed1a3538b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-04-04 17:19:11 +0900

    Improve profiles#update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,8 +9,11 @@ class ProfilesController < ApplicationController
 
   def update(member)
     @member = current_user.member
-    if @member.update(member)
-      current_user.update_auth0_user!(name: @member.name) if @member.previous_changes.key?(:name)
+    @member.assign_attributes(member)
+
+    if @member.valid?
+      current_user.update_auth0_user!(name: @member.name) if @member.attribute_changed?(:name)
+      @member.save!
       redirect_to @member, notice: 'プロフィールを更新しました'
     else
       render :show, status: :unprocessable_entity
