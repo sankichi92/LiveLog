2f0147873bbc7b46a5dae9d94cc90db814ff2b39
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-04-11 15:14:33 +0900

    Add error handler for ActiveRecord::RecordNotFound

--- app/graphql/livelog_schema.rb
+++ app/graphql/livelog_schema.rb
@@ -4,10 +4,22 @@ class LiveLogSchema < GraphQL::Schema
 
   context_class CustomContext
 
+  # region Plugins
+
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
   use GraphQL::Analysis::AST
+  use GraphQL::Execution::Errors
 
-  # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+  # endregion
+
+  # region Error handlers
+
+  rescue_from ActiveRecord::RecordNotFound do |_err, _obj, _args, _ctx, field|
+    raise GraphQL::ExecutionError, "#{field.type.unwrap.graphql_name} not found"
+  end
+
+  # endregion
 end
