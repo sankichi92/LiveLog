3c289e593e63391910d8714d60b43fcf68be4ba3
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-04-12 12:14:08 +0900

    Introduce Types::BaseConnection

--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -3,5 +3,6 @@ module Types
     include GraphQL::Schema::Interface
 
     field_class Types::BaseField
+    connection_type_class Types::BaseConnection
   end
 end
--- app/graphql/types/base_union.rb
+++ app/graphql/types/base_union.rb
@@ -1,4 +1,5 @@
 module Types
   class BaseUnion < GraphQL::Schema::Union
+    connection_type_class Types::BaseConnection
   end
 end
