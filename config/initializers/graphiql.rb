67a1b20e5106e631b155e305d4497d19bca7fd2b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-04-19 12:09:23 +0900

    Update GraphiQL config

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,8 +1,8 @@
 require 'app_auth0_client'
 
 GraphiQL::Rails.config.tap do |config|
-  config.title = 'LiveLog GraphQL API'
-  config.logo = 'LiveLog GraphQL API'
+  config.title = 'LiveLog GraphiQL'
+  config.logo = 'LiveLog GraphiQL'
   config.csrf = false
   config.headers['Authorization'] = lambda do |context|
     cookies = Rails.env.production? ? context.cookies.encrypted : context.cookies
@@ -17,4 +17,48 @@ GraphiQL::Rails.config.tap do |config|
 
     "Bearer #{access_token}"
   end
+  config.initial_query = <<~GRAPHQL
+    # LiveLog GraphiQL へようこそ！
+    #
+    # ここでは LiveLog GraphQL API を試すことができます。
+    # LiveLog GraphQL API については https://github.com/sankichi92/LiveLog/wiki をご覧ください。
+    #
+    query {
+      lives(first: 3) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        edges {
+          cursor
+          node {
+            id
+            date
+            name
+            place
+            songs(first: 3) {
+              totalCount
+              nodes {
+                id
+                order
+                name
+                artist
+                original
+                players {
+                  edges {
+                    instrument
+                    node {
+                      id
+                      joinedYear
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
 end
