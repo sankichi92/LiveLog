34a4751a3c83303ec2eae4533574f0f86d56e968
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-04-23 00:15:13 +0900

    Add graphql resuest spec for default query

--- spec/requests/api/graphql_request_spec.rb
+++ spec/requests/api/graphql_request_spec.rb
@@ -3,71 +3,70 @@ require 'rails_helper'
 RSpec.describe 'graphql request:', type: :request do
   describe 'GET /api/graphql' do
     let(:access_token) { stub_access_token }
-    let(:query) do
-      <<~GRAPHQL
-        query IntrospectionQuery {
-          __schema {
-            queryType { name }
-            mutationType { name }
-            subscriptionType { name }
-            types {
-              ...FullType
-            }
-            directives {
-              name
-              description
-              locations
-              args {
-                ...InputValue
+
+    context 'with introspection query' do
+      let(:query) do
+        <<~GRAPHQL
+          query IntrospectionQuery {
+            __schema {
+              queryType { name }
+              mutationType { name }
+              subscriptionType { name }
+              types {
+                ...FullType
+              }
+              directives {
+                name
+                description
+                locations
+                args {
+                  ...InputValue
+                }
               }
             }
           }
-        }
 
-        fragment FullType on __Type {
-          kind
-          name
-          description
-          fields(includeDeprecated: true) {
+          fragment FullType on __Type {
+            kind
             name
             description
-            args {
+            fields(includeDeprecated: true) {
+              name
+              description
+              args {
+                ...InputValue
+              }
+              type {
+                ...TypeRef
+              }
+              isDeprecated
+              deprecationReason
+            }
+            inputFields {
               ...InputValue
             }
-            type {
+            interfaces {
               ...TypeRef
             }
-            isDeprecated
-            deprecationReason
+            enumValues(includeDeprecated: true) {
+              name
+              description
+              isDeprecated
+              deprecationReason
+            }
+            possibleTypes {
+              ...TypeRef
+            }
           }
-          inputFields {
-            ...InputValue
-          }
-          interfaces {
-            ...TypeRef
-          }
-          enumValues(includeDeprecated: true) {
+
+          fragment InputValue on __InputValue {
             name
             description
-            isDeprecated
-            deprecationReason
+            type { ...TypeRef }
+            defaultValue
           }
-          possibleTypes {
-            ...TypeRef
-          }
-        }
 
-        fragment InputValue on __InputValue {
-          name
-          description
-          type { ...TypeRef }
-          defaultValue
-        }
-
-        fragment TypeRef on __Type {
-          kind
-          name
-          ofType {
+          fragment TypeRef on __Type {
             kind
             name
             ofType {
@@ -88,6 +87,10 @@ RSpec.describe 'graphql request:', type: :request do
                       ofType {
                         kind
                         name
+                        ofType {
+                          kind
+                          name
+                        }
                       }
                     }
                   }
@@ -95,17 +98,68 @@ RSpec.describe 'graphql request:', type: :request do
               }
             }
           }
-        }
-      GRAPHQL
+        GRAPHQL
+      end
+
+      it 'responds 200 without errors' do
+        post api_graphql_path, headers: { authorization: "Bearer #{access_token}" }, params: { query: query }
+        response_body = JSON.parse(response.body)
+
+        expect(response_body.keys).to contain_exactly 'data'
+        expect(response).to have_http_status :ok
+      end
     end
 
-    it 'responds 200' do
-      post api_graphql_path, headers: { authorization: "Bearer #{access_token}" }, params: { query: query }
-      response_body = JSON.parse(response.body)
+    context 'with default query' do
+      let(:query) do
+        <<~GRAPHQL
+          query {
+            lives(first: 3) {
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+              edges {
+                cursor
+                node {
+                  id
+                  date
+                  name
+                  place
+                  songs(first: 3) {
+                    totalCount
+                    nodes {
+                      id
+                      order
+                      name
+                      artist
+                      original
+                      players {
+                        edges {
+                          instrument
+                          node {
+                            id
+                            joinedYear
+                            name
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        GRAPHQL
+      end
 
-      expect(response).to have_http_status :ok
-      expect(response_body).to include 'data'
-      expect(response_body).not_to include 'errors'
+      it 'responds 200 without errors' do
+        post api_graphql_path, headers: { authorization: "Bearer #{access_token}" }, params: { query: query }
+        response_body = JSON.parse(response.body)
+
+        expect(response_body.keys).to contain_exactly 'data'
+        expect(response).to have_http_status :ok
+      end
     end
   end
 end
