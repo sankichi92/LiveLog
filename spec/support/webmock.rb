eed4ecda2ff0a53b35ee52d560347cde0a5faf6e
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-05-09 12:24:17 +0900

    Allow request to chromedriver.storage.googleapis.com

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,5 +1,8 @@
 RSpec.configure do |config|
   config.before :suite do
-    WebMock.disable_net_connect!(allow_localhost: true)
+    WebMock.disable_net_connect!(
+      allow_localhost: true,
+      allow: %w[chromedriver.storage.googleapis.com],
+    )
   end
 end
