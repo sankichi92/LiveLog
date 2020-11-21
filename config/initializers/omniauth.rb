a89e6e7ba6806b4bf89483249f5ce40f6f036883
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-11-21 11:52:38 +0900

    OmniAuth.config.logger = Rails.logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -12,3 +12,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
+
+OmniAuth.config.logger = Rails.logger
