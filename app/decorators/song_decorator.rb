sankichi92/LiveLog

commit ff2b5ee6d332fa7c77cb3caf437dbe8a9cbf2c7a

Bump rubocop from 1.6.1 to 1.7.0 (#1124)

app/decorators/song_decorator.rb
@@ -32,7 +32,7 @@ module SongDecorator
 
   def title_with_original
     if original?
-      safe_join [title, tag.small('（オリジナル曲）')]
+      safe_join[title, tag.small('（オリジナル曲）')]
     else
       title
     end

commit b9c4bd1cdfcda752d6555033b29684ac03657511

Fix a bug caused by #1124

app/decorators/song_decorator.rb
@@ -32,7 +32,7 @@ module SongDecorator
 
   def title_with_original
     if original?
-      safe_join[title, tag.small('（オリジナル曲）')]
+      safe_join([title, tag.small('（オリジナル曲）')])
     else
       title
     end