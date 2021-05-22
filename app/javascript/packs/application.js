6b9ae8a0af91d67b28abc4aa1c9f4a43b659ccb0
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-05-22 18:52:01 +0900

    Replace webpacker with simpacker (#1318)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,8 @@
 import Rails from '@rails/ujs';
 import * as Sentry from '@sentry/browser';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap';
 import '../controllers';
 
-Rails.start();
+Sentry.init({ dsn: 'https://a73e1519070f4bbab6079e78a5801590@sentry.io/1821621' });
 
-Sentry.init({
-  dsn: 'https://a73e1519070f4bbab6079e78a5801590@sentry.io/1821621',
-  release: process.env.HEROKU_SLUG_COMMIT,
-  environment: process.env.NODE_ENV,
-  debug: process.env.NODE_ENV !== 'production',
-});
+Rails.start();
