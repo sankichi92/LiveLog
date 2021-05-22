ebfc4165c941ad11c07c8123007e4e48142c836e
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-05-22 20:04:08 +0900

    Setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 
-const { NODE_ENV } = process.env;
+const { NODE_ENV, USE_WEBPACK_DEV_SERVER } = process.env;
 const isProd = NODE_ENV === 'production';
 
 const entry = {};
@@ -16,7 +16,7 @@ module.exports = {
   mode: isProd ? 'production' : 'development',
   output: {
     path: path.resolve(__dirname, 'public/packs'),
-    publicPath: '/packs/',
+    publicPath: USE_WEBPACK_DEV_SERVER ? '//localhost:3035/packs/' : '/packs/',
     filename: isProd ? '[name]-[contenthash].js' : '[name].js',
   },
   module: {
@@ -43,11 +43,20 @@ module.exports = {
     new WebpackAssetsManifest({
       publicPath: true,
       output: 'manifest.json',
+      writeToDisk: true,
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
   ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/packs/',
+    port: 3035,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
   devtool: 'source-map',
 };
