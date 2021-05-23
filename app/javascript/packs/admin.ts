3b6ec0bb12b2461cc2f51b56a3fb00511a6e5b0b
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-05-24 00:05:20 +0900

    Record user_id on JS Sentry

--- app/javascript/packs/admin.ts
+++ app/javascript/packs/admin.ts
@@ -5,12 +5,16 @@ import 'bootstrap';
 import 'admin-lte';
 import '../controllers/admin';
 
+const rootDataset = document.documentElement.dataset;
 Sentry.init({
   enabled: process.env.NODE_ENV === 'production',
   dsn: 'https://a73e1519070f4bbab6079e78a5801590@sentry.io/1821621',
   environment: process.env.NODE_ENV,
-  release: document.documentElement.dataset.release,
+  release: rootDataset.release,
 });
+if (rootDataset.userId) {
+  Sentry.setUser({ id: rootDataset.userId });
+}
 
 Rails.start();
 
