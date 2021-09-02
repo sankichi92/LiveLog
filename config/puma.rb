f008195ca9203625e4f1e322fe73ce6081bba416
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-09-03 08:22:44 +0900

    Revert "Update config/puma.rb"
    This reverts commit 7bb7801b22e5d242f4f588c8168e8a62a45b2a09.

--- config/puma.rb
+++ config/puma.rb
@@ -32,14 +32,14 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch('WEB_CONCURRENCY', 2)
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app!
+# preload_app!
 
 # https://devcenter.heroku.com/articles/language-runtime-metrics-ruby#add-the-barnes-gem-to-your-application
 before_fork do
