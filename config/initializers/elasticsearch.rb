77c8ec58231f4e28f0a0c41fdbcb9fcb41a0b27a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-11-13 18:11:25 +0900

    Improve ports of docker-compose containers (#1546)

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,4 +2,7 @@
 
 if ENV['ELASTICSEARCH_URL']
   Elasticsearch::Model.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'])
+else
+  docker_port = `docker-compose port elasticsearch 9200`.chomp.split(':').last
+  Elasticsearch::Model.client = Elasticsearch::Client.new(url: "http://localhost:#{docker_port}") if docker_port.present?
 end
