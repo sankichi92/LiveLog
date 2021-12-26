a96900a76aec7d01ff94f5d9e17734111cbc5310
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-12-26 14:19:45 +0900

    Revert action_mailer settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { protocol: :https, host: 'livelog.ku-unplugged.net' }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :login,
+    user_name: 'apikey',
+    password: ENV.fetch('SENDGRID_API_KEY'),
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
