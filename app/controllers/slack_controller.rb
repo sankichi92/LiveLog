e74f69276245271ab01cdd6277a89e708ee1d5e9
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-01-20 19:44:17 +0900

    Allow other host on slack#show redirection

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,6 +4,6 @@ class SlackController < ApplicationController
   before_action :require_current_user
 
   def show
-    redirect_to slack_invitation_url
+    redirect_to slack_invitation_url, allow_other_host: true
   end
 end
