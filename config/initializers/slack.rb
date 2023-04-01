477853626fa42a62fe5a9989ea985d1b9d68e6e7
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2023-04-01 17:52:27 +0900

    Fix slack-client-ruby config

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -4,3 +4,9 @@ Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
   config.logger = Rails.logger
 end
+
+# https://github.com/slack-ruby/slack-ruby-client/issues/415
+Slack::Web.configure do |config|
+  config.ca_file = nil
+  config.ca_path = nil
+end
