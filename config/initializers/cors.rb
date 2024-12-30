1d10c6773aa45d2d51512a6a705e4bb41bed3e7b
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2024-12-30 11:58:25 +0900

    bin/rails app:update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-    resource '/api/graphql', headers: :any, methods: %i[get post put patch delete options head]
+    resource '/api/graphql',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
