0ed91d3438c254a0cd0e35d695452f0077104db6
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2024-12-30 12:15:24 +0900

    Set `config.assets.compile = false` in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,5 +98,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  # Do not fall back to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   Cloudinary.config.secure = true
 end
