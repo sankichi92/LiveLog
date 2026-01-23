f064fa00c70d7993d4212f95c19c9f03e7d6c884
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2026-01-23 09:27:54 +0900

    Remove AdSense (#3085)

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class Donation < ApplicationRecord
-  MONTHLY_COST = 50
-
   belongs_to :member
-
-  def active?(on: Time.zone.today)
-    on <= expires_on
-  end
-
-  def expires_on
-    donated_on + active_duration
-  end
-
-  def active_duration
-    (amount / MONTHLY_COST).months
-  end
 end
